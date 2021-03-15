b7e8257a5ddbc66d2442580af834625291d64aee
monicarivi <momokobcn@gmail.com>
2021-03-15 17:34:05 +0100

    retoques

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Validator;
 use App\Models\Clients;
 use Illuminate\Http\Request;
 
 class ClientsController extends Controller
 {
-  
+
     /**
      * Display a listing of the resource.
      *
@@ -85,61 +86,97 @@ class ClientsController extends Controller
     }
 
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
         //Recoger datos usuario post
-        $json=$request->input('json', null);
-       
-        //Decodificar json
-        $params = json_decode($json);//objeto
-        $params_array = json_decode($json, true);//array
-        
-        //Validar datos
-        $validate = Validator::make($params_array, [
-            'name' =>'required|alpha',
-            'surname' =>'required|alpha',
-            'email' =>'required|email',
-            'password' =>'required',
-            'phone' =>'required|numeric'
-        ]);
+        $json = $request->input('json', null);
 
-        if($validate->fails()){
+        //Decodificar json
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true); //array
+
+        if (!empty($params) && !empty($params_array)) {
+            //limpiar datos
+
+            $params_array = array_map('trim', $params_array);
+
+            //Validar datos
+            $validate = Validator::make($params_array, [
+                'name' => 'required|alpha',
+                'surname' => 'required|alpha',
+                'email' => 'required|email|unique:users',
+                'password' => 'required',
+                'phone' => 'required|numeric'
+            ]);
+  //Comprobar si el usuario existe, duplicado
+            if ($validate->fails()) {
+                //Validacion fallada
+                $data = array(
+
+                    'status' => 'error',
+                    'code' => 404,
+                    'message' => 'El cliente no se ha creado',
+                    'errors' => $validate->errors()
+                );
+            } else {
+                //Validacion ok
+
+                //cifrar contraseña
+                $pwd= password_hash($params->password, PASSWORD_BCRYPT, ['cost' => 4]);
+
+              
+                //Crear cliente
+                $clients = new Clients();
+                $clients->name= $params_array['name'];
+                $clients->surname= $params_array['surname'];
+                $clients->email= $params_array['email'];
+                $clients->password= $pwd;
+                $clients->phone= $params_array['phone'];
+
+
+                var_dump($clients);die();
+
+
+
+
+
+
+                //Mensaje error o no
+                $data = array(
+
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'El cliente  se ha creado  correctamente'
+
+                );
+            }
+        } else {
             $data = array(
 
                 'status' => 'error',
                 'code' => 404,
-                'message' => 'El cliente no se ha creado',
-                'errors' => $validate->errors()
-               );
-        }else{
-            $data = array(
+                'message' => 'Datos  enviados no son correctos'
 
-                'status' => 'success',
-                'code' => 200,
-                'message' => 'El cliente  se ha creado  correctamente'
-                
-               );
+            );
         }
 
 
-        //cifrar contraseña
-        //Comprobar si el usuario existe, duplicado
-        //Crear cliente
-        //Mensaje error o no
-        
 
 
 
 
 
-       
 
 
-       return response()->json($data, $data['code']);
 
+
+
+        return response()->json($data, $data['code']);
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         return "Acción de loggin de usuarios";
     }
 }
